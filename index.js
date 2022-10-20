3a4cca7107d7c1c66f275d08221528fba5b93f75
Pranish <pranish_202000069@smit.smu.edu.in>
2022-10-20 18:21:17 +0530

    Third commit: Updated the memes params router get request

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const server = express()
+const memesList = require("./memeResource")
 
-server.get("/", (request,response) => {
+server.get("/", (request, response) => {
     return response.send("<h2>Hello World</h2>");
 });
 
-server.listen(3000, () =>{
+server.get("/meme", (request, response) => {
+    const list = [...memesList];
+    const randomIndex = Math.floor(Math.random() * list.length);
+    console.log(list[randomIndex]);
+    return response.send(`<img src="${list[randomIndex]}"/>`);
+});
+
+server.get("/meme/:id", (request, response) => {
+    const list = [...memesList];
+    const randomIndex = request.params.id;
+    console.log(list[randomIndex]);
+    return response.send(`<img src="${list[randomIndex]}"/>`);
+});
+
+server.listen(3000, () => {
     console.log("Server started on port 3000");
 });
